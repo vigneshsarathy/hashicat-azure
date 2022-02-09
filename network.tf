f9aadb2ad25f491fd3f6fa43d5c618a7e2c6ff5e
vigneshsarathy <vgs@ramboll.com>
2022-02-09 19:30:05 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -3,7 +3,6 @@ module "network" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = [var.address_space]
-  subnet_names        = "vgs-subnet"
  
   tags = {
     environment = "devops"
